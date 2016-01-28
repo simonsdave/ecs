6abea2656bb650425731a78037c7633eb76db9c2
Dave Simons <simonsdave@gmail.com>
2016-01-28 16:07:32 -0500

    SO unit tests 4 async_docker_remote_api_unit_tests

---
+++ ecs/tests/async_docker_remote_api_unit_tests.py
@@ -0,0 +1,29 @@
+"""This module contains a collection of unit tests which
+validate the ..async_docker_remote_api module.
+"""
+
+import unittest
+import uuid
+
+from ..async_actions import AsyncContainerStart
+
+
+class AsyncContainerStartTestCase(unittest.TestCase):
+
+    def test_ctr_without_async_state(self):
+        container_id = uuid.uuid4().hex
+
+        acs = AsyncContainerStart(container_id)
+
+        self.assertTrue(acs.container_id is container_id)
+
+        self.assertIsNone(acs.async_state)
+
+    def test_ctr_with_async_state(self):
+        container_id = uuid.uuid4().hex
+        async_state = uuid.uuid4().hex
+
+        acs = AsyncContainerStart(container_id, async_state)
+
+        self.assertTrue(acs.container_id is container_id)
+        self.assertTrue(acs.async_state is async_state)
