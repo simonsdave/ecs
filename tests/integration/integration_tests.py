4e07f6376332aebeb5f6eab7e5fc0ba639dcdb6e
Dave Simons <simonsdave@gmail.com>
2016-02-23 23:31:45 -0500

    int test assume docker_remote_api @ 127.0.0.1:4243

--- tests/integration/integration_tests.py
+++ tests/integration/integration_tests.py
@@ -41,6 +41,7 @@ class ServiceConfig(object):
         cp.set(section, 'port', self.port)
         cp.set(section, 'log_level', 'info')
         cp.set(section, 'max_concurrent_executing_http_requests', '250')
+        cp.set(section, 'docker_remote_api', 'http://127.0.0.1:4243')
 
         self.filename = tempfile.mktemp()
         with open(self.filename, 'w+') as fp:
