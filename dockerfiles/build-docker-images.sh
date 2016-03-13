7be53eec3ac7be0bc6ab4a3cf14797d567579625
simonsdave <simonsdave@gmail.com>
2016-03-12 21:17:14 -0500

    couple of bug fixes in build-docker-images.sh

--- dockerfiles/build-docker-images.sh
+++ dockerfiles/build-docker-images.sh
@@ -25,24 +25,26 @@ PASSWORD=${5:-}
 
 if [ ! -r "$SERVICES_TAR_GZ" ]; then
     echo "can't find source dist '$SERVICES_TAR_GZ'" >&2
+    exit 1
 fi
 
 if [ ! -r "$API_DOCS_TAR" ]; then
     echo "can't find api docs tar file '$API_DOCS_TAR'" >&2
+    exit 1
 fi
 
-"$SCRIPT_DIR_NAME/nginx/build-docker-images.sh" \
+"$SCRIPT_DIR_NAME/nginx/build-docker-image.sh" \
     "$USERNAME" \
     "$EMAIL" \
     "$PASSWORD"
 
-"$SCRIPT_DIR_NAME/apidocs/build-docker-images.sh" \
+"$SCRIPT_DIR_NAME/apidocs/build-docker-image.sh" \
     "$API_DOCS_TAR" \
     "$USERNAME" \
     "$EMAIL" \
     "$PASSWORD"
 
-"$SCRIPT_DIR_NAME/nginx/build-docker-images.sh" \
+"$SCRIPT_DIR_NAME/services/build-docker-image.sh" \
     "$SERVICES_TAR_GZ" \
     "$USERNAME" \
     "$EMAIL" \
