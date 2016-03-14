16e63d370fffaacf596239e049f8e02682f838d3
simonsdave <simonsdave@gmail.com>
2016-03-13 21:36:11 -0500

    add qualys ssl lab assessment image to api docs

--- api_docs/build-api-docs.sh
+++ api_docs/build-api-docs.sh
@@ -26,6 +26,8 @@ else
     CREATE_TAR_FILE=0
 fi
 
+DEPLOYMENT_DIR=/usr/share/nginx/ecs/html
+
 if [ $# != 0 ]; then
     echo "usage: `basename $0` [-d] [-t]" >&2
     exit 1
@@ -42,7 +44,6 @@ html_to_raml() {
 
     local RAML="$SCRIPT_DIR_NAME/$BASE_FILENAME.raml"
     local HTML="$SCRIPT_DIR_NAME/$BASE_FILENAME.html"
-    local DEPLOYMENT_DIR=/usr/share/nginx/ecs/html
     local DEPLOYMENT_HTML="$DEPLOYMENT_DIR/$BASE_FILENAME.html"
 
     raml2html "$RAML" > "$HTML"
@@ -88,11 +89,14 @@ rm -f "$SCRIPT_DIR_NAME/*.html"
 for FILENAME in $SCRIPT_DIR_NAME/*.raml; do FILENAME=$(basename "$FILENAME"); html_to_raml ${FILENAME%.*}; done
 
 if [ "$DEPLOY_API_DOCS" == "1" ]; then
+    sudo mkdir -p "$DEPLOYMENT_DIR/images"
+    sudo cp -r "$SCRIPT_DIR_NAME/images" "$DEPLOYMENT_DIR"
+
     echo "Find API docs @ http://127.0.0.1:$(cat /etc/nginx/sites-enabled/default | grep listen | sed -e 's|^\s*listen\s*||g' | sed -e 's|\s*;\s*$||g')"
 fi
 
 if [ "$CREATE_TAR_FILE" == "1" ]; then
     pushd "$SCRIPT_DIR_NAME" > /dev/null
-    tar cvf api_docs.tar *.html
+    tar cvf api_docs.tar *.html images
     popd > /dev/null
 fi
