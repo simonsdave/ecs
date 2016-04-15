f26e6b1c013918686a85e72521c3b4f9b1fa033b
simonsdave <simonsdave@gmail.com>
2016-04-15 17:15:02 -0500

    make run_load_test.sh more independant

--- tests/load/run_load_test.sh
+++ tests/load/run_load_test.sh
@@ -1,8 +1,48 @@
 #!/usr/bin/env bash
 
+if [ "-v" == "${1:-}" ]; then
+    VERBOSE=1
+    shift
+else
+    VERBOSE=0
+fi
+
 SCRIPT_DIR_NAME="$( cd "$( dirname "$0" )" && pwd )"
 
-rm "$SCRIPT_DIR_NAME/raw_data.tsv" >& /dev/null
+if [ $# != 0 ] && [ $# != 1 ]; then
+    echo "usage: `basename $0` [-v] [ecs endpoint]" >&2
+    exit 1
+fi
+
+if [ $# == 0 ]; then
+    ECS_ADDRESS=127.0.0.1
+    ECS_PORT=9448
+
+    ECS_ENDPOINT="http://$ECS_ADDRESS:$ECS_PORT"
+
+    ECS_CONFIG_FILE=$(mktemp 2> /dev/null || mktemp -t DAS)
+    echo "[ecs]" >> "$ECS_CONFIG_FILE"
+    echo "address=$ECS_ADDRESS" >> "$ECS_CONFIG_FILE"
+    echo "port=$ECS_PORT" >> "$ECS_CONFIG_FILE"
+    echo "log_level=info" >> "$ECS_CONFIG_FILE"
+    echo "max_concurrent_executing_http_requests=250" >> "$ECS_CONFIG_FILE"
+    echo "docker_remote_api=http://172.17.42.1:2375" >> "$ECS_CONFIG_FILE"
+    echo "docker_remote_api_connect_timeout=3000" >> "$ECS_CONFIG_FILE"
+    echo "docker_remote_api_request_timeout=300000" >> "$ECS_CONFIG_FILE"
+
+    ECS_LOG_FILE=$(mktemp 2> /dev/null || mktemp -t DAS)
+    ecservice.py --config="$ECS_CONFIG_FILE" >& /dev/null &
+    ECS_PID=$!
+
+    if [ $VERBOSE == 1 ]; then
+        echo "Started ECS - PID = ECS_PID; config file  '$ECS_CONFIG_FILE'"
+    fi
+else
+    ECS_ENDPOINT=$1
+fi
+
+LOG_FILE=$(mktemp 2> /dev/null || mktemp -t DAS)
+LOCUST_OUTPUT_FILE=$(mktemp 2> /dev/null || mktemp -t DAS)
 
 locust \
     --locustfile="$SCRIPT_DIR_NAME/locustfile.py" \
@@ -10,7 +50,17 @@ locust \
     --num-request=1000 \
     --clients=25 \
     --hatch-rate=5 \
-    --host=http://127.0.0.1:8448 \
-    --logfile="$SCRIPT_DIR_NAME/raw_data.tsv"
+    --host=$ECS_ENDPOINT \
+    --logfile="$LOG_FILE" \
+    >& "$LOCUST_OUTPUT_FILE"
+
+echo "Find log file @ '$LOG_FILE'"
+echo "Find locust output @ '$LOCUST_OUTPUT_FILE'"
+
+"$SCRIPT_DIR_NAME/analyze_load_test_tsv.py" < "$LOG_FILE"
+
+if [ "${ECS_PID:-}" != "" ]; then
+    kill $ECS_PID
+fi
 
 exit 0
