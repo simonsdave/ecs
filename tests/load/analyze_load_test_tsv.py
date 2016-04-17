eda0da41bb52ac7fe9a2a17b94f08b23adf5b2e0
simonsdave <simonsdave@gmail.com>
2016-04-17 09:03:52 -0500

    analyze_load_test_tsv.py cmd line arg parsing

--- tests/load/analyze_load_test_tsv.py
+++ tests/load/analyze_load_test_tsv.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import datetime
+import optparse
 import re
 import sys
 
@@ -116,6 +117,24 @@ class Main(object):
         print '=' * len(overall_title)
 
 
+class CommandLineParser(optparse.OptionParser):
+
+    def __init__(self):
+        optparse.OptionParser.__init__(
+            self,
+            'usage: %prog [options] <concurrency>',
+            description='This utility analyzes load test results')
+
+    def parse_args(self, *args, **kwargs):
+        (clo, cla) = optparse.OptionParser.parse_args(self, *args, **kwargs)
+        if 0 != len(cla):
+            self.error('invalid command line args')
+        return (clo, cla)
+
+
 if __name__ == '__main__':
+    clp = CommandLineParser()
+    (clo, cla) = clp.parse_args()
+
     main = Main()
     main.analyze()
