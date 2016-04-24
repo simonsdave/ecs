6767bab01dfeb9fb47f802eb9b3f56b91cd8c52e
simonsdave <simonsdave@gmail.com>
2016-04-24 12:35:58 -0500

    flake8'ing

--- tests/load/matplotlibsample.py
+++ tests/load/matplotlibsample.py
@@ -2,8 +2,10 @@
 
 import numpy as np
 # http://stackoverflow.com/questions/2801882/generating-a-png-with-matplotlib-when-display-is-undefined
-import matplotlib
-matplotlib.use('Agg')
+# http://stackoverflow.com/questions/29073802/matplotlib-cannot-find-configuration-file-matplotlibrc
+# import matplotlib
+# print matplotlib.get_configdir()
+# matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
 # evenly sampled time at 200ms intervals
@@ -12,4 +14,4 @@ t = np.arange(0., 5., 0.2)
 # red dashes, blue squares and green triangles
 plt.plot(t, t, 'r--', t, t**2, 'bs', t, t**3, 'g^')
 
-plt.savefig('/vagrant/foo.png') 
+plt.savefig('/vagrant/foo.png')
