e81701b4262e79d2e3b7c0f786c9654f0727e15d
Dave Simons <simonsdave@gmail.com>
2016-08-15 21:09:04 -0400

    upgrade dev env to docker 1.12

--- dev_env/provision.sh
+++ dev_env/provision.sh
@@ -8,12 +8,23 @@ set -e
 
 apt-get update -y
 
-apt-get install -y docker.io
-sed -i -e 's|#DOCKER_OPTS="--dns 8.8.8.8 --dns 8.8.4.4"|DOCKER_OPTS="-H tcp://172.17.42.1:2375 -H unix:///var/run/docker.sock"|g' /etc/default/docker
+apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" | tee /etc/apt/sources.list.d/docker.list
+apt-get update
+apt-get install -y docker-engine
+echo 'waiting for docker0 network to start '
+while ! ifconfig | grep docker0 >& /dev/null
+do
+        echo '.'
+        sleep 1
+done
+echo 'docker0 network started'
+sed -i -e 's|#DOCKER_OPTS="--dns 8.8.8.8 --dns 8.8.4.4"|DOCKER_OPTS="-H tcp://172.17.0.1:2375 -H unix:///var/run/docker.sock"|g' /etc/default/docker
 usermod -aG docker vagrant
 service docker restart
 
 apt-get install -y git
+
 apt-get install -y python-virtualenv
 apt-get install -y python-dev
 apt-get build-dep -y python-crypto
