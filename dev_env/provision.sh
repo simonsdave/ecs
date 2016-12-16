b9c60d62770992374baadd0bb1c19abc12fe00b1
Dave Simons <simonsdave@gmail.com>
2016-12-16 13:35:01 -0500

    fix dev env google cloud sdk provision error

--- dev_env/provision.sh
+++ dev_env/provision.sh
@@ -85,7 +85,7 @@ chmod a+x /usr/local/bin/jq
 export CLOUD_SDK_REPO=cloud-sdk-`lsb_release -c -s`
 echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | tee /etc/apt/sources.list.d/google-cloud-sdk.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-apt-get update && apt-get install google-cloud-sdk
+apt-get update -y && apt-get install -y google-cloud-sdk
 
 cp /vagrant/.vimrc ~vagrant/.vimrc
 chown vagrant:vagrant ~vagrant/.vimrc
