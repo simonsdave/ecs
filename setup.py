b38711a3433d866aa58c62e056fbb930e2d2b828
Dave Simons <simonsdave@gmail.com>
2017-04-02 13:20:34 -0400

    tornado 4.4.2->4.4.3; tor_async_util 1.12.0->1.13.0

--- setup.py
+++ setup.py
@@ -46,8 +46,8 @@ setup(
         # using tornado.curl_httpclient.CurlAsyncHTTPClient
         'pycurl>=7.19.5.1',
         'semantic-version==2.6.0',
-        'tor-async-util==1.12.0',
-        'tornado==4.4.2',
+        'tor-async-util==1.13.0',
+        'tornado==4.4.3',
     ],
     dependency_links=[
         'https://github.com/simonsdave/tor-async-util/tarball/v1.12.0#egg=tor-async-util-1.12.0',
